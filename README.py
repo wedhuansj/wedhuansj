57c70f55187945bbcfb25ce8210addb19ba897e7
wedhuansj <nguyenbuihongquan@gmail.com>
2024-11-06 20:29:37 +0700

    Update README.md

--- README.py
+++ README.py
@@ -1,10 +1,1 @@
-X, Y, H = map(int, input().split())
-count = 0
-current_height = 0
-while current_height < H:
-    count += 1
-    current_height += X
-    if current_height >= H:
-        break
-    current_height -= Y
-print(count)
+a
